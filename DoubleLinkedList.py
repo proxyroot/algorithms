49b8a41cdf179d53393d8988a3de300ecb95872d
Afroze Khan <itsmeafroze@gmail.com>
2016-11-02 11:32:52 -0400

    Double linked list

--- DoubleLinkedList.py
+++ DoubleLinkedList.py
@@ -1,15 +1,16 @@
 """
     Author: Afroze Khan
     Created: 11/2/2016
-    Description: Program to implement single linked list
+    Description: Program to implement Double linked list
 """
 
 class Node(object):
     nxt = None
+    prev = None
     def __init__(self, value):
         self.value = value
 
-class SingleLinkedList(object):
+class DoubleLinkedList(object):
 
     def __init__(self):
         self.head = None
@@ -22,28 +23,33 @@ class SingleLinkedList(object):
         current = self.head
         while current.nxt != None:
             current = current.nxt
+        node.prev = current
         current.nxt = node
 
     def delete(self, value):
         if self.head:
             current = self.head
             if current.value == value:
+                self.head.nxt.prev = None
                 self.head = self.head.nxt
                 return
             while current.nxt and current.nxt.value != value:
                 current = current.nxt
+            if current.nxt.nxt:
+                current.nxt.nxt.prev = current
             current.nxt = current.nxt.nxt
 
     def display(self):
         current = self.head
         while current.nxt:
-            print current.value, "->"
+            print current.value, "<=>"
             current = current.nxt
         print current.value
 
     def sort(self):
         pass
-objLinkList = SingleLinkedList()
+
+objLinkList = DoubleLinkedList()
 values = [1, 4, 2, 10, 6,5,3]
 for val in values:
     node = Node(val)
@@ -58,6 +64,9 @@ objLinkList.delete(10)
 objLinkList.display()
 print "Deleting 3"
 objLinkList.delete(3)
+objLinkList.display()
+print "Deleting 5"
+objLinkList.delete(5)
 objLinkList.display()
 
 """
@@ -88,4 +97,8 @@ Deleting 3
 2 ->
 6 ->
 5
+Deleting 5
+4 ->
+2 ->
+6
 """
